b91f0d7ab20157223d3dfd97f85b359e0d13d514
navyd <dhjnavyd@gmail.com>
2024-07-07 10:03:34 +0800

    chore: Reenable lazyvim on vscode and add jj as escape key

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
--- vscode-neovim Neovim configuration: https://github.com/vscode-neovim/vscode-neovim#neovim-configuration
-if vim.g.vscode then
-  -- vscode extension
-else
-  require("config.lazy")
-end
+require("config.lazy")
