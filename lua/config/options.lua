f5cf6b0b5ec30930fc6b95b9cb4638090da7245c
navyd <dhjnavyd@gmail.com>
2024-07-27 16:22:58 +0800

    chore: Disable autoformat on save

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable autoformat global
+-- use `<leader>uf` to toggle autoformat
+-- [Disable autoformat for some buffers](https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers)
+vim.g.autoformat = false
