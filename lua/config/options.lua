930b1dc757d12d874cdcc37a578a6fe12d70cef4
navyd <dhjnavyd@gmail.com>
2024-12-21 16:11:39 +0800

    chore: format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,7 +14,7 @@ vim.g.autoformat = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard:append('unnamedplus')
+  vim.opt.clipboard:append("unnamedplus")
 
   -- Fix "waiting for osc52 response from terminal" message
   -- https://github.com/neovim/neovim/issues/28611
@@ -25,21 +25,21 @@ vim.schedule(function()
     local function my_paste(_)
       return function(_)
         local content = vim.fn.getreg('"')
-        return vim.split(content, '\n')
+        return vim.split(content, "\n")
       end
     end
 
     vim.g.clipboard = {
-      name = 'OSC 52',
+      name = "OSC 52",
       copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
       },
       paste = {
         -- No OSC52 paste action since wezterm doesn't support it
         -- Should still paste from nvim
-        ['+'] = my_paste('+'),
-        ['*'] = my_paste('*'),
+        ["+"] = my_paste("+"),
+        ["*"] = my_paste("*"),
       },
     }
   end
@@ -47,7 +47,7 @@ end)
 
 -- 在win上配置默认使用pwsh
 -- 参考: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-if jit.os == 'Windows' then
+if jit.os == "Windows" then
   local win_sh = nil
   if vim.fn.executable("pwsh") then
     win_sh = "pwsh"
@@ -58,4 +58,3 @@ if jit.os == 'Windows' then
     LazyVim.terminal.setup(win_sh)
   end
 end
-
