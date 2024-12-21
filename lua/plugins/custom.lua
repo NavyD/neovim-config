110776c9f7b0ffbd6043b8d0a0fbb1d89741c2db
navyd <dhjnavyd@gmail.com>
2024-12-21 22:43:48 +0800

    chore: Enable git blame inline hint

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -4,4 +4,13 @@ return {
   -- ...lazy/LazyVim/lua/lazyvim/plugins/extras/util/chezmoi.lua:60: attempt to concatenate a nil value
   { "xvzc/chezmoi.nvim", cond = jit.os ~= "Windows" },
   { "alker0/chezmoi.vim", cond = jit.os ~= "Windows" },
+  {
+    "lewis6991/gitsigns.nvim",
+    -- https://github.com/lewis6991/gitsigns.nvim#installation--usage
+    opts = {
+      -- Toggle with `:Gitsigns toggle_current_line_blame`
+      -- 启用git blame line
+      current_line_blame = true,
+    },
+  },
 }
