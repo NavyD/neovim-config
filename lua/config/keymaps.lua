c9c692d612b237c08ed1d56c3b832d7df35124c0
navyd <dhjnavyd@gmail.com>
2024-12-23 20:16:28 +0800

    feat: Add tmux.nvim instead of nvim-tmux-navigation plugin
    参考[Compatibility with vim-yoink or other yank-related
    plugins](https://github.com/aserowy/tmux.nvim/issues/88)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,14 +3,3 @@
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
--- 修复alexghergh/nvim-tmux-navigation在插件中配置keys无效的问题
--- 虽然这个两个插件名称不同但可以用这个方法修复
--- [nvim-tmux-navigation Neovim](https://github.com/alexghergh/nvim-tmux-navigation#neovim)
--- [Sometimes not work for vim-tmux-navigator keybinding. #4109](https://github.com/LazyVim/LazyVim/discussions/4109)
-local nvim_tmux_nav = require("nvim-tmux-navigation")
-vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,15 +1,81 @@
--- https://github.com/alexghergh/nvim-tmux-navigation#neovim
--- 配置中使用config=function()，但lazy.nvim插件建议使用[opts setup](https://lazy.folke.io/spec#spec-setup)，
--- 另外这个插件的setup key需要在keymaps.lua中配置
--- 参考https://github.com/LazyVim/LazyVim/discussions/4109#discussioncomment-10456484
---
--- 注意：需要单独配置tmux参考https://github.com/alexghergh/nvim-tmux-navigation#tmux
--- 可以使用[.tmux](https://github.com/gpakosz/.tmux)框架管理tmux
+-- [tmux integration for nvim features pane movement and resizing from within nvim.](https://github.com/aserowy/tmux.nvim)
+-- 参考[Compatibility with vim-yoink or other yank-related plugins #88](https://github.com/aserowy/tmux.nvim/issues/88#issuecomment-2259591730)
+-- 参考https://github.com/moetayuko/nvimrc/blob/master/lua/plugins/tmux.lua
+-- 目前仅替换了按键从`A[lt]`->`C[trl]`
 return {
   {
-    "alexghergh/nvim-tmux-navigation",
+    "aserowy/tmux.nvim",
+    event = "VeryLazy",
     opts = {
-      -- disable_when_zoomed = true,
+      navigation = {
+        enable_default_keybindings = false,
+      },
+      resize = {
+        enable_default_keybindings = false,
+      },
+      copy_sync = {
+        enable = true,
+        sync_registers_keymap_put = false,
+        sync_registers_keymap_reg = false,
+      },
+    },
+    config = function(_, opts)
+      require("tmux").setup(opts)
+      if vim.env.TMUX then
+        LazyVim.on_load("which-key.nvim", function()
+          local reg = require("which-key.plugins.registers")
+          local expand = reg.expand
+          function reg.expand()
+            require("tmux.copy").sync_registers()
+            return expand()
+          end
+        end)
+
+        if LazyVim.has("yanky.nvim") then
+          LazyVim.on_load("yanky.nvim", function()
+            local yanky = require("yanky")
+            local put = yanky.put
+            function yanky.put(type, is_visual, callback)
+              require("tmux.copy").sync_registers()
+              return put(type, is_visual, callback)
+            end
+          end)
+        end
+      end
+    end,
+    keys = {
+      {
+        "<C-H>",
+        function()
+          require("tmux").move_left()
+        end,
+        desc = "Go to left window",
+        remap = true,
+      },
+      {
+        "<C-J>",
+        function()
+          require("tmux").move_bottom()
+        end,
+        desc = "Go to lower window",
+        remap = true,
+      },
+      {
+        "<C-K>",
+        function()
+          require("tmux").move_top()
+        end,
+        desc = "Go to upper window",
+        remap = true,
+      },
+      {
+        "<C-L>",
+        function()
+          require("tmux").move_right()
+        end,
+        desc = "Go to right window",
+        remap = true,
+      },
     },
   },
 }
