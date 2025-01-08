aa237f193a608cadf1faf1747d7efba339bc1964
navyd <dhjnavyd@gmail.com>
2025-01-09 00:50:54 +0800

    chore: Disable auto-dark-mode plugin on termux

--- lua/plugins/autodarkmode.lua
+++ lua/plugins/autodarkmode.lua
@@ -1,6 +1,9 @@
+---@type LazyPluginSpec
 return {
   -- [auto-dark-mode.nvim](https://github.com/f-person/auto-dark-mode.nvim)
   "f-person/auto-dark-mode.nvim",
+  -- 在termux中无效禁止加载
+  cond = not string.find(vim.env.PREFIX, "com.termux"),
   opts = {
     update_interval = 1000,
     set_dark_mode = function()
