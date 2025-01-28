04829f2505fed81e778df3d78e75ae38d04e4278
navyd <dhjnavyd@gmail.com>
2025-01-28 12:11:49 +0800

    chore: Disable spell check in markdown,text filetypes
    在vscode或nvim中打开md文件会默认启用`set spell`，在中文环境中非常难用，默认关闭

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- [how to disable spellcheck in markdown file? #4021](https://github.com/LazyVim/LazyVim/discussions/4021)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "text" },
+    callback = function()
+      vim.opt_local.spell = false
+    end,
+})
+
