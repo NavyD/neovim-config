365633656fc440ddbec25a447178e08d7fbc6c7f
navyd <dhjnavyd@gmail.com>
2025-02-06 17:18:56 +0800

    feat: Add helpview.nvim plugin

---
+++ lua/plugins/helpview.lua
@@ -0,0 +1,6 @@
+-- [A hackable & fancy vimdoc/help file viewer for Neovim](https://github.com/OXY2DEV/helpview.nvim)
+---@type LazyPluginSpec
+return {
+  "OXY2DEV/helpview.nvim",
+  lazy = false,
+}
