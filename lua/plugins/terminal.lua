eab61ed2f5c7518878a972496a9a4f3a99546953
navyd <dhjnavyd@gmail.com>
2025-11-14 12:39:34 +0800

    feat: Add unnest.nvim

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,8 @@
+---@type LazyPluginSpec[]
+return {
+  {
+    -- [No worry about nested Nvim in Nvim terminal](https://github.com/brianhuster/unnest.nvim)
+    "brianhuster/unnest.nvim",
+    lazy = false,
+  },
+}
