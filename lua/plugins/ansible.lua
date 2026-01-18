5f8244830f43f0d63a7aeb9d479a81e0e2dd31b1
navyd <dhjnavyd@gmail.com>
2026-01-18 22:51:03 +0800

    feat: Add ansible plugin

---
+++ lua/plugins/ansible.lua
@@ -0,0 +1,45 @@
+-- ansible 不支持 windows 平台作为控制节点
+if vim.fn.has('win32') == 1 then
+  return {}
+end
+
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/ansible.lua
+---@type LazyPluginSpec[]
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "ansible-lint" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ansiblels = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-ansible",
+    ft = { "yaml" },
+    keys = {
+      {
+        "<leader>ta",
+        function()
+          require("ansible").run()
+        end,
+        ft = "yaml.ansible",
+        desc = "Ansible Run Playbook/Role",
+        silent = true,
+      },
+    },
+  },
+  -- 添加 ansible lint
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        ["yaml.ansible"] = { "ansible_lint" },
+      },
+    },
+  },
+}
