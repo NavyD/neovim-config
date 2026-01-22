0e897f57f3e085d45ebac5ba4468382f681929a3
navyd <dhjnavyd@gmail.com>
2026-01-22 16:27:54 +0800

    chore: Add env MASON_GITHUB_DOWNLOAD_URL_TEMPLATE to speed up

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,4 +4,17 @@ return {
   {
     import = "plugins.lang",
   },
+  {
+    "mason-org/mason.nvim",
+    ---@module 'mason'
+    ---@type MasonSettings
+    -- https://github.com/mason-org/mason.nvim#default-configuration
+    opts = {
+      github = {
+        -- 默认 `https://github.com/%s/releases/download/%s/%s`
+        -- 通常可配置为 `https://gh-proxy.com/https://github.com/%s/releases/download/%s/%s`
+        download_url_template = vim.env.MASON_GITHUB_DOWNLOAD_URL_TEMPLATE,
+      },
+    },
+  },
 }
