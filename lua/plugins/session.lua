b87ae57b7f5f03252e38d27b8e8d69a85817afbf
navyd <dhjnavyd@gmail.com>
2026-01-23 20:14:27 +0800

    feat(persisted): Only save session if buffers are present

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -12,13 +12,27 @@ return {
     "olimorris/persisted.nvim",
     version = "3",
     lazy = false,
-    cond = true,
     opts = {
       -- 不要自动加载，可以使用 `nvim -c '=require("persisted").load()'` 加载
       autoload = false,
       use_git_branch = true,
       allowed_dirs = {}, -- Table of dirs that the plugin will start and autoload from
       ignored_dirs = {}, -- Table of dirs that are ignored for starting and autoloading
+      -- Only save session if a minimum number of buffers are present
+      ---@type fun(): boolean
+      should_save = function()
+        -- Ref: https://github.com/folke/persistence.nvim/blob/166a79a55bfa7a4db3e26fc031b4d92af71d0b51/lua/persistence/init.lua#L46
+        local bufs = vim.tbl_filter(function(b)
+          if vim.bo[b].buftype ~= "" or vim.tbl_contains({ "gitcommit", "gitrebase", "jj" }, vim.bo[b].filetype) then
+            return false
+          end
+          return vim.api.nvim_buf_get_name(b) ~= ""
+        end, vim.api.nvim_list_bufs())
+        if #bufs < 1 then
+          return false
+        end
+        return true
+      end,
     },
     keys = {
       {
