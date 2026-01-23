d36468b1efd4868335303e4e815de217dcfb34dc
navyd <dhjnavyd@gmail.com>
2026-01-23 16:36:59 +0800

    chore: Add custom types for lint,mason

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,18 @@
+---@module 'lint'
+-- https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/plugins/linting.lua#L79
+---@class LazyVimLinterCtx
+---@field filename string
+
+-- https://github.com/mfussenegger/nvim-lint/blob/ca6ea12daf0a4d92dc24c5c9ae22a1f0418ade37/lua/lint.lua#L157
+---@class LazyVimLinter: lint.Linter
+---@field condition fun(ctx: LazyVimLinterCtx):boolean
+
+-- https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/plugins/linting.lua#L7
+---@class LazyVimLintSettings
+---@field events? string[]
+---@field linters_by_ft? table<string, string[]>
+---@field linters? table<string, LazyVimLinter | fun():lint.Linter>
+
 ---@module 'lazy'
 ---@type LazyPluginSpec[]
 return {
@@ -6,9 +21,12 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    ---@module 'mason'
-    ---@type MasonSettings
     -- https://github.com/mason-org/mason.nvim#default-configuration
+    -- 在 lazyvim 中定义的类型，实现参考：
+    -- https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/plugins/lsp/init.lua#L279
+    ---@module 'mason'
+    ---@alias LazyVimMasonSettings MasonSettings | { ensure_installed: string[] }
+    ---@type LazyVimMasonSettings
     opts = {
       github = {
         -- 默认 `https://github.com/%s/releases/download/%s/%s`
