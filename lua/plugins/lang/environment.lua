0045e685e9d739658445874b0a5ba97a0068f746
navyd <dhjnavyd@gmail.com>
2026-02-18 12:58:24 +0800

    chore: Disable misel on vscode

--- lua/plugins/lang/environment.lua
+++ lua/plugins/lang/environment.lua
@@ -7,7 +7,7 @@ return {
     dependencies = { "nvim-neotest/nvim-nio" },
     lazy = true,
     event = "VeryLazy",
-    cond = vim.fn.executable("mise") == 1,
+    cond = vim.g.vscode ~= 1 and vim.fn.executable("mise") == 1,
     ---@type misel.EnvOpts
     opts = { load_env_immediately = vim.env.MISE_SHELL == nil },
   },
