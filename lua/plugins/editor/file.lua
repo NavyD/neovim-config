259ab769bc738f678549b6b09f2b3cf62c83d3ee
navyd <dhjnavyd@gmail.com>
2026-07-06 19:54:49 +0800

    fix(nsuda): use dir=stdpath('config') for local plugin loading

--- lua/plugins/editor/file.lua
+++ lua/plugins/editor/file.lua
@@ -1,12 +1,12 @@
 ---@type LazySpec
 return {
   {
-    -- [suda is a plugin to read or write files with sudo command](https://github.com/lambdalisue/vim-suda)
-    "lambdalisue/suda.vim",
-    init = function()
-      -- 自动读写需要的使用sudo的文件 https://github.com/lambdalisue/vim-suda#smart-edit
-      vim.g.suda_smart_edit = 1
-    end,
+    -- [nsuda is a plugin to read or write files with sudo command]
+    dir = vim.fn.stdpath("config"),
+    name = "nsuda.nvim",
+    opts = {
+      smart_edit = true,
+    },
   },
   {
     -- Neovim file explorer: edit your filesystem like a buffer
