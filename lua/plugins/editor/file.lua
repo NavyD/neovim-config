f54386316cba6741f2d4923525f3f77334fc2a60
navyd <dhjnavyd@gmail.com>
2026-07-06 20:14:57 +0800

    merge: resolve conflicts with feat/nsuda changes

--- lua/plugins/editor/file.lua
+++ lua/plugins/editor/file.lua
@@ -1,21 +1,19 @@
 ---@type LazySpec
 return {
   {
-    -- [nsuda — sudo plugin for Neovim](https://github.com/lambdalisue/vim-suda)
-    dir = vim.fn.stdpath("config") .. "/lua/utils/nsuda",
-    opts = { smart_edit = true },
+    -- [nsuda is a plugin to read or write files with sudo command](https://github.com/NavyD/nsuda.nvim)
+    "navyd/nsuda.nvim",
+    opts = {
+      smart_edit = true,
+    },
   },
   {
     -- Neovim file explorer: edit your filesystem like a buffer
-    -- https://github.com/stevearc/oil.nvim
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
-    -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     keys = { { "<leader>f-", "<CMD>Oil<CR>", { desc = "Open parent directory" } } },
   },
