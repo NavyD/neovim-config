d2c290ca57c52789b655491085bcec1ff6a23831
navyd <dhjnavyd@gmail.com>
2026-07-21 08:29:05 +0800

    feat: add yamllint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,10 +2,12 @@
 -- https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/plugins/linting.lua#L79
 ---@class LazyVimLinterCtx
 ---@field filename string
+---@field dirname string
 
 -- https://github.com/mfussenegger/nvim-lint/blob/ca6ea12daf0a4d92dc24c5c9ae22a1f0418ade37/lua/lint.lua#L157
 ---@class LazyVimLinter: lint.Linter
----@field condition fun(ctx: LazyVimLinterCtx):boolean
+---@field condition? fun(ctx: LazyVimLinterCtx):boolean
+---@field prepend_args string[]?
 
 -- https://github.com/LazyVim/LazyVim/blob/c64a61734fc9d45470a72603395c02137802bc6f/lua/lazyvim/plugins/linting.lua#L7
 ---@alias LazyVimLintOptsFtLinters string[] | { [integer]: string, _compound_ft: boolean?}
@@ -13,6 +15,7 @@
 ---@field events? string[]
 ---@field linters_by_ft? table<string, LazyVimLintOptsFtLinters>
 ---@field linters? table<string, LazyVimLinter | fun():lint.Linter>
+---#配置 linter 注意是以 linter 名而非 filetype 如对于 yaml 使用 yamllint 时配置为 `{linters={yamllint={}}}`
 
 ---@module 'lazyvim.plugins.lsp'
 ---@class LazyVimLspOpts: PluginLspOpts
---
+++ lua/plugins/lang/yaml.lua
@@ -0,0 +1,63 @@
+-- 检查 yamllint 的配置文件是否存在，如果不存在则返回 `-d relaxed` 避免过多警告
+-- https://yamllint.readthedocs.io/en/stable/configuration.html#configuration
+local function get_yamllint_prepend_args()
+  -- 如果存在任意的配置文件
+  if
+    vim
+      .iter({ "$XDG_CONFIG_HOME/yamllint/config", "~/.config/yamllint/config", "$YAMLLINT_CONFIG_FILE" })
+      :map(vim.fn.expand)
+      :any(function(v)
+        return vim.uv.fs_stat(v) ~= nil
+      end)
+  then
+    return
+  end
+
+  -- 如果当前或父目录中存在默认配置文件
+  local conf_markers = { ".yamllint", ".yamllint.yaml", ".yamllint.yml" }
+  local conf_paths = vim.fs.find(conf_markers, {
+    upward = true,
+    path = vim.uv.cwd(),
+    limit = 1,
+    stop = vim.uv.os_homedir(),
+  })
+  if #conf_paths > 0 then
+    return
+  end
+
+  local json_enc = vim.json.encode
+  local gen_data = function()
+    local ec = vim.b.editorconfig or {}
+    local data = {
+      extends = "relaxed",
+      rules = {
+        ["line-length"] = { max = ec.max_line_length or 120 },
+      },
+    }
+    return json_enc(data)
+  end
+  -- lint.Linter.args 的类型定义是 (string|fun():string)[]，
+  -- 允许 动态参数生成 和 工作目录感知。
+  return { "-d", gen_data }
+end
+
+---@type LazySpec
+return {
+  {
+    "mason-org/mason.nvim",
+    ---@type LazyVimMasonOpts
+    opts = { ensure_installed = { "yamllint" } },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    ---@param opts LazyVimLintOpts
+    opts = function(_, opts)
+      opts.linters_by_ft.yaml = { "yamllint" }
+
+      opts.linters = opts.linters or {}
+      local linter = opts.linters.yamllint or {}
+      linter.prepend_args = get_yamllint_prepend_args()
+      opts.linters.yamllint = linter
+    end,
+  },
+}
