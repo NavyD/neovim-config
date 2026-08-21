37d85bb0c26e58c5849b03f5ac921d7c17dce587
navyd <dhjnavyd@gmail.com>
2026-08-21 19:18:12 +0800

    feat: add nvim-early-retirement to auto close buffers

--- lua/plugins/editor/buf.lua
+++ lua/plugins/editor/buf.lua
@@ -1,5 +1,22 @@
 ---@type LazySpec
--- [sleuth.vim: Heuristically set buffer options](https://github.com/tpope/vim-sleuth)
 return {
-  "tpope/vim-sleuth",
+  {
+    -- [sleuth.vim: Heuristically set buffer options](https://github.com/tpope/vim-sleuth)
+    "tpope/vim-sleuth",
+  },
+  {
+    -- Send buffers into early retirement by automatically closing them
+    -- after x minutes of inactivity.
+    -- https://github.com/chrisgrieser/nvim-early-retirement
+    "chrisgrieser/nvim-early-retirement",
+    event = "VeryLazy",
+    ---@module 'early-retirement'
+    ---@type opts
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      retirementAgeMins = 15,
+      notificationOnAutoClose = true,
+      minimumBufferNum = 10,
+    },
+  },
 }
